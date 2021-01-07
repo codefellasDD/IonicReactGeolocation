e9e4344cdea892f751cebacf260cd6d4bb443d0e
Maik Rengsberger <codefellasDD@users.noreply.github.com>
2021-01-07 16:50:02 +0100

    Add watchPosition

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,34 +6,37 @@ import {
   IonToolbar,
   IonGrid,
   IonCol,
-  IonRow,
+  IonRow
 } from "@ionic/react";
 import React from "react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 import GeolocationButton from "../components/GeolocationButton";
+import GeolocationPage from "../components/TestComp";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Geolocation</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Geolocation</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+  // return (
+  //   <IonPage>
+  //     <IonHeader>
+  //       <IonToolbar>
+  //         <IonTitle>Geolocation</IonTitle>
+  //       </IonToolbar>
+  //     </IonHeader>
+  //     <IonContent>
+  //       <IonHeader collapse="condense">
+  //         <IonToolbar>
+  //           <IonTitle size="large">Geolocation</IonTitle>
+  //         </IonToolbar>
+  //       </IonHeader>
 
-        <div className="container">
-          <GeolocationButton />
-        </div>
-      </IonContent>
-    </IonPage>
-  );
+  //       <div className="container">
+  //         <GeolocationButton />
+
+  //       </div>
+  //     </IonContent>
+  //   </IonPage>
+  // );
+  return <GeolocationPage />;
 };
 
 export default Home;
